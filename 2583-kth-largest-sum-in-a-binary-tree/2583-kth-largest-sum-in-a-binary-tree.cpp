efdfa59e1094325d9325481b98b74782c76c8487
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2024-10-23 09:52:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        vector<long long> sum;
+        if(root==nullptr){
+            return 0;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int levelSum=0;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                levelSum+=temp->val;
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                }
+            }
+            cout<<levelSum<<endl;
+            sum.push_back(levelSum);
+        }   
+
+        sort(sum.begin(),sum.end());
+        if (k <= sum.size()) {
+            return sum[sum.size()-k]; 
+        }
+        return -1;
+    }
+};
