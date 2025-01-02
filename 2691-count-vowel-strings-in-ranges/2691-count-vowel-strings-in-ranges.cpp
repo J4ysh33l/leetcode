44ce6a8d0773bd4eef93406714859021fce89c5c
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2025-01-02 14:11:40 +0530

    Time: 27 ms (13.88%), Space: 74.6 MB (22.42%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    bool isVowel(char chr){
+        if(chr == 'a' ||chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        // first initialize a vector dp
+        // traverse the vector words and increment dp if words[i] starts and ends with vowel
+        // for queries dp[r]-dp[l-1] if (l-1)<0 then return dp[r];
+        // add these results in ans
+        vector<int> ans;
+        vector<int> dp(words.size()+1);
+        dp[0]=0;
+        for(int i=0;i<words.size();i++){
+            string word= words[i];
+            if(isVowel(word[0]) && isVowel(word[word.size()-1])){
+                dp[i+1]=dp[i]+1;
+            }else{
+                dp[i+1]=dp[i];
+            }
+        }
+        for(auto x: queries){
+            ans.push_back(dp[x[1]+1]-dp[x[0]]);
+        }
+
+        return ans;
+    }
+};
