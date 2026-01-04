710f3804c01b64ac3623c4030f4ff0087b6524d4
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2026-01-05 00:03:34 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.cpp
@@ -0,0 +1,13 @@
+// Last updated: 05/01/2026, 00:03:35
+class Solution {
+public:
+    std::vector<int> findWordsContaining(std::vector<std::string>& words, char x) {
+        std::vector<int> result;
+        for (int i = 0; i < words.size(); ++i) {
+            if (words[i].find(x) != std::string::npos) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
