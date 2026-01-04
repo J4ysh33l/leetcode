53d2c2f544458142c956274563f94c346a4cce05
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2026-01-05 00:03:04 +0530

    Create 3396-ValidWord

---
+++ 3396-ValidWord/3396-ValidWord.cpp
@@ -0,0 +1,25 @@
+// Last updated: 05/01/2026, 00:03:04
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        if (n < 3) return false;
+
+        int vowels = 0, consonants = 0;
+        string vowelList = "aeiouAEIOU";
+
+        for (char c : s) {
+            if (isalpha(c)) {
+                if (vowelList.find(c) != string::npos) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            } else if (!isdigit(c)) {
+                return false; // invalid character
+            }
+        }
+
+        return vowels >= 1 && consonants >= 1;
+    }
+};
