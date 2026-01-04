b0d0dd89c60fae5cdd6f86e30229922c4a52629c
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2026-01-05 00:02:32 +0530

    Create 3522-FindThePowerOfKSizeSubarraysI

---
+++ 3522-FindThePowerOfKSizeSubarraysI/3522-FindThePowerOfKSizeSubarraysI.cpp
@@ -0,0 +1,18 @@
+// Last updated: 05/01/2026, 00:02:32
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans(n - k + 1, -1);
+        int score = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i && nums[i - 1] + 1 == nums[i])
+                score++;
+            else
+                score = 0;
+            if (score >= k - 1)
+                ans[i - k + 1] = nums[i];
+        }
+        return ans;
+    }
+};
