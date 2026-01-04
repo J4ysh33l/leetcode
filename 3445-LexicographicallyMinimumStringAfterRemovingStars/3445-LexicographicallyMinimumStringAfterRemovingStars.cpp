b4cfa2445c6d6182f9f6d878358f122e92be0ebd
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2026-01-05 00:02:58 +0530

    Create 3445-LexicographicallyMinimumStringAfterRemovingStars

---
+++ 3445-LexicographicallyMinimumStringAfterRemovingStars/3445-LexicographicallyMinimumStringAfterRemovingStars.cpp
@@ -0,0 +1,22 @@
+// Last updated: 05/01/2026, 00:02:58
+class Solution {
+public:
+    string clearStars(string s) {
+    priority_queue<pair<char,int>, vector<pair<char,int>>, greater<pair<char,int>>> minh;
+    unordered_map<int,bool> mpp;
+        for (int i = 0; i < s.size(); i++) {
+            if(s[i] == '*') {
+                mpp[-minh.top().second] = true;
+                minh.pop();
+            } else {
+                minh.push({s[i], -i});
+            }
+        }
+        string res;
+        for(int i = 0; i < s.size(); i++) {
+            if(mpp.count(i) || s[i] == '*') continue;
+            res += s[i];
+        }
+        return res;
+    }
+};
