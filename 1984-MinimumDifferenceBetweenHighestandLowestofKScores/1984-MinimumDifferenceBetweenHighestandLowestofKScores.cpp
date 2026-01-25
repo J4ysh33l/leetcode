167c6582bbcd33f48d33a7d210975623d1a00ca1
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2026-01-25 23:57:08 +0530

    Create 1984-MinimumDifferenceBetweenHighestandLowestofKScores

---
+++ 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.cpp
@@ -0,0 +1,13 @@
+// Last updated: 25/01/2026, 23:57:07
+1class Solution {
+2public:
+3    int minimumDifference(vector<int>& nums, int k) {
+4        int n = nums.size();
+5        sort(nums.begin(), nums.end());
+6        int ans = nums[k - 1] - nums[0];
+7        for(int i = 0; i + k <= n; i ++){
+8            ans = min(ans, nums[i + k - 1] - nums[i]);
+9        }
+10        return ans;
+11    }
+12};
