3f3d8e18b08497dbc55df1cd3d770e0d910d9250
Jaysheel Bhatt <97089971+J4ysh33l@users.noreply.github.com>
2026-02-28 23:11:38 +0530

    Create 1680-ConcatenationofConsecutiveBinaryNumbers

---
+++ 1680-ConcatenationofConsecutiveBinaryNumbers/1680-ConcatenationofConsecutiveBinaryNumbers.cpp
@@ -0,0 +1,18 @@
+// Last updated: 28/02/2026, 23:11:38
+1class Solution {
+2 public:
+3  int concatenatedBinary(int n) {
+4    constexpr int kMod = 1'000'000'007;
+5    long ans = 0;
+6
+7    for (int i = 1; i <= n; ++i)
+8      ans = ((ans << numberOfBits(i)) % kMod + i) % kMod;
+9
+10    return ans;
+11  }
+12
+13 private:
+14  int numberOfBits(int n) {
+15    return log2(n) + 1;
+16  }
+17};
